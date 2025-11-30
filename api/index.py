ef0dd3e33157e99aad7eeb3847f804881c8ddcf7
aminmatlo <devkarim60@gmail.com>
2025-11-30 04:56:02 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def MajorLogin(payload, headers):
+    url = "https://loginbp.ggpolarbear.com/MajorLogin"
 
-@app.route('/about')
-def about():
-    return 'About'
+    headers = dict(headers)
+    headers.pop("Host", None)
+    headers.pop("Content-Length", None)
+
+    response = requests.post(url, headers=headers, data=payload, verify=False)
+    return response.content
+
+
+@app.route('/MajorLogin', methods=['POST'])
+def MajorLoginProxy():
+    payload = request.get_data()
+    headers = request.headers
+
+    response_data = MajorLogin(payload, headers)
+
+    return response_data
